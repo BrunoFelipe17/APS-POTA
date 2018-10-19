72c385a36b5b2e5416311bbd8cdeaffd59dcb832
Bruno Felipe <bruno.felipe7170@gmail.com>
2018-10-19 01:10:59 -0300

    Final Commit. Thanks god!

--- data_analysis/analysis.R
+++ data_analysis/analysis.R
@@ -1,20 +1,21 @@
 library(ggplot2)
 library(dplyr)
 
-sort_data = read.csv('csv/sort_data.csv')
+sort_data = read.csv('csv/average.csv')
 
 str(sort_data)
+print("Oi")
 
 size_vector <- sort_data$Size
 size_factor <- factor(size_vector)
 size <- size_factor
-comparison_vector_test <- sort_data$Comparison_1
+comparison_vector_test <- sort_data$Comparison_average
 comparison_factor_test <- factor(comparison_vector_test)
-comparisons = factor(sort_data$Comparison_1)
+comparisons = factor(sort_data$Comparison_average)
 
-swaps_vector_test <- sort_data$Swaps
+swaps_vector_test <- sort_data$Swaps_average
 swaps_factor_test <- factor(comparison_vector_test)
-swaps = factor(sort_data$Swaps)
+swaps = factor(sort_data$Swaps_average)
 
 
 ggplot(sort_data, aes(x = size, y = comparisons, color = Name)) +
@@ -27,6 +28,107 @@ ggplot(sort_data, aes(x = size, y = swaps, color = Name)) +
   geom_line(aes(group = Name)) +
   ggtitle("Swaps Graph")
 
+size_5_comp <- sort_data %>%
+  filter(Size == 5) %>%
+  select(-c(Swaps_average))
 
+ggplot(size_5_comp, aes(x = Name, y = Comparison_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Comparison size 5")
+  
+size_5_swap <- sort_data %>%
+  filter(Size == 5) %>%
+  select(-c(Comparison_average))
+
+ggplot(size_5_swap, aes(x = Name, y = Swaps_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Swaps size 5")
   
   
+size_10_comp <- sort_data %>%
+  filter(Size == 10) %>%
+  select(-c(Swaps_average))
+
+ggplot(size_10_comp, aes(x = Name, y = Comparison_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Comparison size 10")
+
+size_10_swap <- sort_data %>%
+  filter(Size == 5) %>%
+  select(-c(Comparison_average))
+
+ggplot(size_10_swap, aes(x = Name, y = Swaps_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Swaps size 10")
+
+
+
+size_50_comp <- sort_data %>%
+  filter(Size == 50) %>%
+  select(-c(Swaps_average))
+
+ggplot(size_50_comp, aes(x = Name, y = Comparison_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Comparison size 50")
+
+size_50_swap <- sort_data %>%
+  filter(Size == 50) %>%
+  select(-c(Comparison_average))
+
+ggplot(size_50_swap, aes(x = Name, y = Swaps_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Swaps size 50")
+
+
+
+size_100_comp <- sort_data %>%
+  filter(Size == 100) %>%
+  select(-c(Swaps_average))
+
+ggplot(size_100_comp, aes(x = Name, y = Comparison_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Comparison size 100")
+
+size_100_swap <- sort_data %>%
+  filter(Size == 100) %>%
+  select(-c(Comparison_average))
+
+ggplot(size_100_swap, aes(x = Name, y = Swaps_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Swaps size 100")
+
+
+
+size_1000_comp <- sort_data %>%
+  filter(Size == 1000) %>%
+  select(-c(Swaps_average))
+
+ggplot(size_1000_comp, aes(x = Name, y = Comparison_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Comparison size 1000")
+
+size_1000_swap <- sort_data %>%
+  filter(Size == 1000) %>%
+  select(-c(Comparison_average))
+
+ggplot(size_1000_swap, aes(x = Name, y = Swaps_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Swaps size 1000")
+
+
+
+size_10000_comp <- sort_data %>%
+  filter(Size == 10000) %>%
+  select(-c(Swaps_average))
+
+ggplot(size_10000_comp, aes(x = Name, y = Comparison_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Comparison size 10000")
+
+size_10000_swap <- sort_data %>%
+  filter(Size == 10000) %>%
+  select(-c(Comparison_average))
+
+ggplot(size_10000_swap, aes(x = Name, y = Swaps_average , color = Name)) +
+  geom_bar(stat = 'identity', width=0.3, aes(fill = Name)) +
+  ggtitle("Swaps size 10000")
--- src/App/Main.java
+++ src/App/Main.java
@@ -7,43 +7,48 @@ public class Main {
 //        Generate random numbers and save to a file
 //        rd.generate("random_numbers");
 
-        SortingAlgorithms sa = new SortingAlgorithms("random_numbers");
+        SortingAlgorithms sa = new SortingAlgorithms("crescent");
 //        SortingAlgorithms sa = new SortingAlgorithms("crescent");
 
 
-        sa.bubble(5);
-        sa.bubble(10);
-        sa.bubble(50);
-        sa.bubble(100);
-        sa.bubble(1000);
         sa.bubble(10000);
-
-        sa.selection(5);
-        sa.selection(10);
-        sa.selection(50);
-        sa.selection(100);
-        sa.selection(1000);
         sa.selection(10000);
-
-        sa.insertion(5);
-        sa.insertion(10);
-        sa.insertion(50);
-        sa.insertion(100);
-        sa.insertion(1000);
         sa.insertion(10000);
-
-        sa.merge(5);
-        sa.merge(10);
-        sa.merge(50);
-        sa.merge(100);
-        sa.merge(1000);
         sa.merge(10000);
-
-        sa.quick(5);
-        sa.quick(10);
-        sa.quick(50);
-        sa.quick(100);
-        sa.quick(1000);
         sa.quick(10000);
+
+//        sa.bubble(10);
+//        sa.bubble(50);
+//        sa.bubble(100);
+//        sa.bubble(1000);
+//        sa.bubble(10000);
+//
+//
+//        sa.selection(10);
+//        sa.selection(50);
+//        sa.selection(100);
+//        sa.selection(1000);
+//        sa.selection(10000);
+//
+//
+//        sa.insertion(10);
+//        sa.insertion(50);
+//        sa.insertion(100);
+//        sa.insertion(1000);
+//        sa.insertion(10000);
+//
+//
+//        sa.merge(10);
+//        sa.merge(50);
+//        sa.merge(100);
+//        sa.merge(1000);
+//        sa.merge(10000);
+//
+//
+//        sa.quick(10);
+//        sa.quick(50);
+//        sa.quick(100);
+//        sa.quick(1000);
+//        sa.quick(10000);
     }
 }
